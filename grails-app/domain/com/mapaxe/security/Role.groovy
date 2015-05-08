2ea851dc9e3aa7ea94163cf3d268841a80260ef1
Antonio Ruiz <isc.antonio.ruiz@gmail.com>
2015-05-07 20:03:57 -0500

    Se configuran las dominios a auditar

--- grails-app/domain/com/mapaxe/security/Role.groovy
+++ grails-app/domain/com/mapaxe/security/Role.groovy
@@ -1,6 +1,8 @@
 package com.mapaxe.security
 
 class Role {
+    transient bitacoraService
+    static auditable = [handlersOnly:true]
 
 	String authority
 
@@ -11,4 +13,49 @@ class Role {
 	static constraints = {
 		authority blank: false, unique: true
 	}
+
+    def onSave = {newState ->
+        String descripcion = ''
+        Long identificador = 0
+        newState.each{key, value ->
+            descripcion = descripcion + ' * ' + key + " : " + value + '\n'
+            if(key.equals('id')){
+                identificador = value
+            }
+        }
+        withNewSession {
+            bitacoraService.guardarAccion('Crear', descripcion, this.domainClass.getName(), identificador)
+        }
+    }
+
+    def onDelete = {oldState ->
+        String descripcion = ''
+        Long identificador = 0
+        oldState.each{key, value ->
+            descripcion = descripcion + ' * ' + key + " : " + value + '\n'
+            if(key.equals('id')){
+                identificador = value
+            }
+        }
+        withNewSession {
+            bitacoraService.guardarAccion('Eliminar', descripcion, this.domainClass.getName(), identificador)
+        }
+    }
+
+    def onChange = { oldMap,newMap ->
+        String descripcion = ''
+        Long identificador = 0
+        oldMap.each({ key, value ->
+            if(value != newMap[key]) {
+
+                descripcion = descripcion + ' * ' + key + ' cambio de ' + value + ' a ' + newMap[key] + '\n'
+            }
+            if(newMap['id']){
+                identificador = value
+            }
+        })
+        withNewSession {
+            bitacoraService.guardarAccion('Editar', descripcion, this.domainClass.getName(), identificador)
+        }
+    }
 }
--- grails-app/domain/com/mapaxe/security/User.groovy
+++ grails-app/domain/com/mapaxe/security/User.groovy
@@ -3,6 +3,8 @@ package com.mapaxe.security
 class User {
 
 	transient springSecurityService
+    transient bitacoraService
+    static auditable = [handlersOnly:true]
 
 	String username
 	String password
@@ -39,4 +41,55 @@ class User {
 	protected void encodePassword() {
 		password = springSecurityService?.passwordEncoder ? springSecurityService.encodePassword(password) : password
 	}
+
+
+    def onSave = {newState ->
+        String descripcion = ''
+        Long identificador = 0
+        newState.each{key, value ->
+            descripcion = descripcion + ' * ' + key + " : " + value + '\n'
+            if(key.equals('id')){
+                identificador = value
+            }
+        }
+        withNewSession {
+                bitacoraService.guardarAccion('Crear', descripcion, this.domainClass.getName(), identificador)
+        }
+    }
+
+    def onDelete = {oldState ->
+        String descripcion = ''
+        Long identificador = 0
+        oldState.each{key, value ->
+            descripcion = descripcion + ' * ' + key + " : " + value + '\n'
+            if(key.equals('id')){
+                identificador = value
+            }
+        }
+        withNewSession {
+            bitacoraService.guardarAccion('Eliminar', descripcion, this.domainClass.getName(), identificador)
+        }
+    }
+
+    def onChange = { oldMap,newMap ->
+        String descripcion = ''
+        Long identificador = 0
+        oldMap.each({ key, value ->
+            if(value != newMap[key]) {
+
+                descripcion = descripcion + ' * ' + key + ' cambio de ' + value + ' a ' + newMap[key] + '\n'
+            }
+            if(newMap['id']){
+                identificador = value
+            }
+        })
+        withNewSession {
+            bitacoraService.guardarAccion('Editar', descripcion, this.domainClass.getName(), identificador)
+        }
+    }
+
+    @Override
+    public String toString() {
+        return username
+    }
 }
